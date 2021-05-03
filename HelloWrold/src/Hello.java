b1b995ab3eb7b266bfe6517ded8cbe42b2fd7d3b
Patil <yogpatil@bmc.com>
2021-05-03 10:41:38 +0530

    New Branch Created and Commit Code

--- HelloWrold/src/Hello.java
+++ HelloWrold/src/Hello.java
@@ -26,4 +26,22 @@ class test1 {
 	System.out.println("Division of 2 nos is "+ f);
 
 	}
+	class test2 {
+		test1(){
+			System.out.println("Contructor called");
+		}
+		void show1() {
+		int a=10;
+		int b=20;
+		//float x=1.9;
+		int c=a + b ;
+		int d=a -b ;
+		float e=a*b;
+		int f=a/b;
+		System.out.println("Addition of 2 nos is : "+ c);
+		System.out.println("Subtraction of 2 nos is "+ d);
+		System.out.println("Multiplication of 2 nos is "+ e);
+		System.out.println("Division of 2 nos is "+ f);
+
+		}
 }
